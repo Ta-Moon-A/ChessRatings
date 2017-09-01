2ffaa18a02cb20a5ac6ace3651709aed83991e36
Tamo Tkhlashidze <tkhlashidzetamo0@gmail.com>
2017-09-01 11:47:58 +0400

    Update processdata.js

--- scripts/processdata.js
+++ scripts/processdata.js
@@ -35,6 +35,10 @@ var data = {
                     fullname: "კაშმაძე",
                     unit: 'ba'
                 },
+                tamart : {
+                   fullname: "თამო",
+                   unit: 'internal'          
+                }
              }
         };
 
